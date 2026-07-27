77a87863a95da58f8b04f591905d9ebb5540af0f
Kartik_2105 <135512254+KARTIK785643@users.noreply.github.com>
2026-07-27 15:40:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api-restcountries': {
+        target: 'https://api.restcountries.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api-restcountries/, '')
+      },
+      '/restcountries-v3': {
+        target: 'https://restcountries.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/restcountries-v3/, '')
+      }
+    }
+  }
 })
